54c7f27e8782c710670a21ea8b2b0a9a092d5ec6
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-04-23 17:56:38 +0900

    Transaction hash 추가

--- src/transaction.js
+++ src/transaction.js
@@ -1,21 +1,23 @@
+const CryptoJS = require("crypto-js");
+
 class TxOut {
   constructor(address, amount){
     this.address = address;
     this.amount = amount;
   }
-}
+};
 
 class TxIn {
-  // uTxOutID
+  // uTxOutId
   // uTxOutIndex
   // Signature
-}
+};
 
 class Transaction {
   // id
   // txIns[]
   // txOuts[]
-}
+};
 
 class UTxOut {
   constructor(uTxOutId, uTxOutIndex, address, amount){
@@ -24,7 +26,17 @@ class UTxOut {
     this.address = address;
     this.amount = amount;
   }
-}
-
+};
 
 const uTxOuts = [];
+
+const getTxId = tx => {
+  const txInContent = tx.txIns
+    .map(txIn => txIn.uTxOutId + txIn.uTxOutIndex)
+    .reduce((a, b) => a + b, "");
+
+  const txOutContent = tx.txOuts
+    .map(txOut => txOut.address + txOut.amount)
+    .reduce((a, b) => a + b, "");
+  return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
