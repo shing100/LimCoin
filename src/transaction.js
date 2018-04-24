4e786ae44cfbfc236dd178e258dc622996cb9c2b
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2018-04-24 17:54:05 +0900

    add elliptic and utiils hex-to-string and Signing the transaction 인풋은 이전의 아웃풋임

--- src/transaction.js
+++ src/transaction.js
@@ -1,4 +1,8 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+  elliptic = require('elliptic'),
+  utils = require("./utils");
+
+const ec = new elliptic.ec("secp256k1");
 
 class TxOut {
   constructor(address, amount){
@@ -20,9 +24,9 @@ class Transaction {
 };
 
 class UTxOut {
-  constructor(uTxOutId, uTxOutIndex, address, amount){
-    this.uTxOutId = uTxOutId;
-    this. uTxOutIndex = uTxOutIndex;
+  constructor(txOutId, txOutIndex, address, amount){
+    this.txOutId = txOutId;
+    this.txOutIndex = txOutIndex;
     this.address = address;
     this.amount = amount;
   }
@@ -39,4 +43,21 @@ const getTxId = tx => {
     .map(txOut => txOut.address + txOut.amount)
     .reduce((a, b) => a + b, "");
   return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
+  return uTxOutList.find(uTxOut => uTxOut.txOutId === uTxOutId && uTxOut.txOutIndex === txOutIndex);
+};
+
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+  const txIn = tx.txIns[txInIndex];
+  const dataToSign = tx.id;
+  // find Tx
+  const referencedUTxOut = findUTxOut(txIn.txOutId, tx.txOutIndex, uTxOuts);
+  if(referencedUTxOut === null){
+    return;
+  }
+  const key = ec.keyFromPrivate(privateKey, "hex");
+  const signature = tuils.toHexString(key.sign(dataToSign).toDER());
+  return signature;
 };
